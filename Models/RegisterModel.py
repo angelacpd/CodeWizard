1bf9be2d86c042828e1cb9bbcb24b79e1c88f84d
Angela Crepaldi <angelacrepaldi@gmail.com>
2019-11-16 03:21:30 -0300

    Hashing passwords using bcrypt.

--- Models/RegisterModel.py
+++ Models/RegisterModel.py
@@ -1,5 +1,6 @@
 import pymongo
 from pymongo import MongoClient
+import bcrypt
 
 class RegisterModel:
     def __init__(self):
@@ -8,7 +9,12 @@ class RegisterModel:
         self.Users = self.db.users
 
     def insert_user(self, data):
+        hashed = bcrypt.hashpw(data.password.encode(), bcrypt.gensalt())
 
-        id1 = self.Users.insert({"username": data.username, "name": data.name, "password": data.password,
+        id1 = self.Users.insert({"username": data.username, "name": data.name, "password": hashed,
                                 "email": data.email})
         print("uid is", id1)
+        myuser = self.Users.find_one({"username": data.username})
+
+        if bcrypt.checkpw("avocado1".encode(), myuser["password"]):
+            print("this matches")
